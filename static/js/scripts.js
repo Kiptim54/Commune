31f3d4fea1d7e23a1455152036ba3c91faaae127
Kiptim <kiptim54@gmail.com>
2018-05-27 20:38:48 +0300

    add index not extending base

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
-    if('background-color'=='white'){
+    if($("body").css("background","rgb(255,255,255)" )){
         $("a").css("color","rgb(255,255,255)");
-        $("nav").css("background-color","rgb(51, 153, 153)");
-    };    
+        $("#linkhere").css("color","rgb(51, 153, 153)");
+        $("nav").css("background","rgb(51, 153, 153)");
+  
+    }
+    else{
+       alert('noth')
+    }
+   
+    
 });
+
