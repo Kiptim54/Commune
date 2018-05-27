8c088a5540c45e617ecfc27e8ce78b73186f7334
Kiptim <kiptim54@gmail.com>
2018-05-27 21:01:48 +0300

    add landing page nav works

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-    if($("body").css("background","rgb(255,255,255)" )){
-        $("a").css("color","rgb(255,255,255)");
+    if($("nav").css("background","rgb(255,255,255)" )){
+        $("nav a").css("color","rgb(255,250,255)");
         $("#linkhere").css("color","rgb(51, 153, 153)");
         $("nav").css("background","rgb(51, 153, 153)");
   
