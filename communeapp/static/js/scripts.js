c8ac7dc058c1d2372a9b8789b1b13b4705986fda
Kiptim <kiptim54@gmail.com>
2018-05-27 14:19:20 +0300

    added jquery

---
+++ communeapp/static/js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    if('background-color'=='white'){
+        $("a").css("color","rgb(255,255,255)");
+        $("nav").css("background-color","rgb(51, 153, 153)");
+    };    
+});
