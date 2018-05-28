487ddae19eae591d53777bceff425ad92550fe3a
Kiptim <kiptim54@gmail.com>
2018-05-29 00:35:23 +0300

    added change logout

--- commune/urls.py
+++ commune/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('communeapp.urls')),
     url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
+    url(r'^logout/$', views.logout,{"next_page": '/'}, name='logout'),
 
 ]
